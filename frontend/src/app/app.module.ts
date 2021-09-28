017ca4b7716d8d524105ccd7fa7d7dacc190446f
sabrine123456 <binabenn@outlook.fr>
2021-09-28 13:22:42 +0200

    ajouter register et navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatDialogModule,
+    RouterModule.forRoot([
+    {
+      path: "register", component: RegisterComponent
+    }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
